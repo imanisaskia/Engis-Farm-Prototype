f90111f122d60406e6574fb3a7c1a0eee1ec5084
Saskia Imani <imani.saskia@gmail.com>
2019-03-18 18:33:50 +0700

    Added Productive attribute to FarmAnimal.

--- Farm Animals/FarmAnimal.hpp
+++ Farm Animals/FarmAnimal.hpp
@@ -5,19 +5,21 @@
 
 class FarmAnimal {
     private:
-        int I, J;       // animal position
-        int Hunger;     // animal hunger level: 0...10; if >5, hungry
+        int I, J;           // animal position
+        int Hunger;         // animal hunger level: 0...10; if >5, hungry
+        bool Productive;    // animal's availibility to produce
 
     public:
-        FarmAnimal(int i, int j);   // creates new animal with hunger = 0 and position I, J
+        FarmAnimal(int i, int j);   // creates new animal with hunger = 0; position I, J; Productive = false
 
-        int getI();         // returns animal row position
-        int getJ();         // returns animal column position
-        int getHunger();    // returns animal hunger
+        int getI();             // returns animal row position
+        int getJ();             // returns animal column position
+        int getHunger();        // returns animal hunger
+        bool getProductive();    // returns animal's availability to produce
 
         void Move();        // move an animal one grid
         void GetHungrier(); // increase hunger
-        void Eat();         // animal eats grass from its grid
+        void Eat();         // animal eats grass from its grid, hunger = 0, animal turns productive
 };
 
 #endif

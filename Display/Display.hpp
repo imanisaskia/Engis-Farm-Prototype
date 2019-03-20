369c0d1d7e3ee609995a88d3411e6526c3e41236
Karina Iswara <44571042+kariswr@users.noreply.github.com>
2019-03-20 12:50:14 +0700

    Update Display.hpp
    added some method

--- Display/Display.hpp
+++ Display/Display.hpp
@@ -15,22 +15,33 @@ class Display {
 		//string input is the name of the external file
 		//Command is set to ""
 
-		Clear();
+		bool checkLand(int i, int j, int type);
+		//Check Land or not
+	
+		bool checkFacility(int i, int j, int type);
+		//Check Facility
+	
+		void modifyGrassy(int i, int j);
+		//modify Grassy
+	
+		void modifyUsedTruck(int i, int j);
+	
+		void Clear();
 		//Makes all Grid as default (empty)
 
-		checkAnimal(AnimalArray<T>);
+		bool checkAnimal(AnimalArray<T>);
 
-		updateMap();
+		void updateMap();
 		//Check what's in every grid of the map
 
-		Print();
+		void Print();
 		//Print every Grid in Map and what's in it with specified layout
 
-		printSentences(std::string);
+		void printSentences(std::string);
 		//	Print any sentences from the game that's need to be printed in a specified area
 		// e.g : print sound of the animals
 
-		std::string getCommand();		
+		void std::string getCommand();		
 		//read User input
 
     private:
